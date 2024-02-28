6b01562d5a9717f8771e74ac66c822a7485c21bb
2311ajay <92317294+2311ajay@users.noreply.github.com>
2024-02-28 20:36:35 +0800

    Removed unwanted imports

--- src/main/java/com/simplekafka/controller/OrderController.java
+++ src/main/java/com/simplekafka/controller/OrderController.java
@@ -1,7 +1,6 @@
 package com.simplekafka.controller;
 
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
